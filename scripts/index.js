6cbd9482fed75efca91a2a0d16ba5574cfe3c4a0
Martin6000 <martinprado4022@gmail.com>
2025-12-23 12:00:35 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,16 @@ function actualizarBarra() {
 }
 
 window.addEventListener("scroll", actualizarBarra);
+
+const boton = document.querySelector("butondown");
+let direction = 1;
+let position = 0;
+
+function rebote() {
+    position += direction;
+    if(position > 5 || position < 0) direction *= -1;
+    boton.style.transform = `translateY(-${position}px)`;
+    requestAnimationFrame(rebote);
+}
+
+rebote();
